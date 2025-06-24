e0f133966b09af171e9efecbb4ce3958f0a58883
Frankincense <frankincensewelsey@gmail.com>
2025-06-24 12:31:56 +0300

    [feature]: Business model added with supporting methods

---
+++ server/models/business.py
@@ -0,0 +1,33 @@
+from server.config import db
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import Column, Integer, String, DateTime,func
+from sqlalchemy.orm import validates, relationship
+from sqlalchemy.ext.hybrid import hybrid_property
+
+class Business(db.Model, SerializerMixin):
+    __tablename__ = 'businesses'
+
+    id = Column(Integer(), primary_key=True)
+    slug = Column(String(), nullable=False)
+    email = Column(String())
+    logo_url = Column(String())
+    _password_hash = Column(String())
+    createdAt = Column(DateTime(), server_default=func.now())
+    updatedAt = Column(DateTime(), onupdate=func.now())
+
+    def __repr__(self):
+        return f'Business: {self.id}, {self.slug}'
+    
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Cannot access this attribute')
+    
+    @password_hash.setter
+    def password_hash(self, value):
+        from server.app import bcrypt
+        password_hash = bcrypt.generate_password_hash(value.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        from server.app import bcrypt
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
